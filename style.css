879bb4019485cf02721c7c02b02d5c18cd4705d7
wealicx <144695058+wealicx@users.noreply.github.com>
2024-08-23 19:20:56 -0300

    alunos

---
+++ style.css
@@ -0,0 +1,85 @@
+*{
+    margin: 0;
+    padding: 0;
+}
+.poppins-regular {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+ 
+  .poppins-bold {
+    font-family: "Poppins", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  body{
+    font-family: "Poppins", sans-serif;
+  }
+ 
+.cabecalho{
+    background-color: #e6a41f;
+    color: #FFFFFF;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 24px 0;
+}
+.cabecalho-imagem{
+width: 30%;
+padding: 0 15px;
+
+}
+.div-cabecalho-logo{
+    display: flex;
+}
+.cabecalho-lista-item{
+    display: inline-block;
+    margin: 0 22px;
+    font-size: 24px;
+}
+.cabecalho-titulo{
+    padding: 20px 0;
+}
+.escola-imagem{
+    width: 20%;
+}
+.escola{
+    background-image:linear-gradient(#fa6210, #D8E9EC);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 24px 0;
+}
+.escola-titulo{
+    padding: 24px 0;
+}
+.escola-texto-um{
+    padding: 24px 0;
+}
+.escola-div-conteudo{
+    width: 35%;
+}
+.estudante-imagem{
+    width: 220px;
+}
+.estudante-icone{
+    width: 24px;
+}
+.estudante-div{
+    text-align: center;
+    padding: 16px 0;
+}
+.estudante-titulo{
+    text-align: center;
+    padding: 50px 0;
+}
+.estudante-todos{
+    display: grid;
+grid-template-columns: 40% 65% 30%;
+}
+.estudante{
+    background-color: #24d629;
+}
